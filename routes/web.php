847bc29eafb70559986bf4f450c5d721835ddb57
dev-promtheus98 <ozh98@protonmail.com>
2020-06-01 03:01:07 +0000

    Implementing authentication system

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Gloudemans\Shoppingcart\Facades\Cart;
 
@@ -26,10 +27,12 @@ Route::get('/boutique/{slug}', 'ProductController@show')->name('products.show');
 Route::get('/search', 'ProductController@search')->name('products.search');
 
 // Cart Routes
-Route::get('/panier', 'CartController@index')->name('cart.index');
-Route::post('/panier/ajouter', 'CartController@store')->name('cart.store');
-Route::patch('/panier/{rowId}', 'CartController@update')->name('cart.update');
-Route::delete('/panier/{rowId}', 'CartController@destroy')->name('cart.destroy');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/panier', 'CartController@index')->name('cart.index');
+    Route::post('/panier/ajouter', 'CartController@store')->name('cart.store');
+    Route::patch('/panier/{rowId}', 'CartController@update')->name('cart.update');
+    Route::delete('/panier/{rowId}', 'CartController@destroy')->name('cart.destroy');
+});
 
 Route::get('/videpanier', function () {
     Cart::destroy();
@@ -37,11 +40,16 @@ Route::get('/videpanier', function () {
 });
 
 // Checkout Routes
-Route::get('/paiement', 'CheckoutController@index')->name('checkout.index');
-Route::post('/paiement', 'CheckoutController@store')->name('checkout.store');
-Route::get('/merci', 'CheckoutController@thankYou')->name('checkout.thankYou');
-
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/paiement', 'CheckoutController@index')->name('checkout.index');
+    Route::post('/paiement', 'CheckoutController@store')->name('checkout.store');
+    Route::get('/merci', 'CheckoutController@thankYou')->name('checkout.thankYou');
+});
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
